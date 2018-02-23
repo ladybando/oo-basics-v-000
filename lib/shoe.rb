286f45c50cf5b6cfbdaf2d1c7abe3417f58ec0d5
Ayana Bando <ayanaflax@yahoo.com>
2018-02-23 18:37:30 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,10 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
   def initialize(brand)
-    @brand =(brand)
+    @brand = brand
   end
-
+  def cobble
+  end
 end
